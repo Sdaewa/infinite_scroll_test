553810433904155642656a9ef5dd5279e0e6d877
Luis Ramirez <sdaewa22@gmail.com>
2020-10-19 15:27:25 +0100

    authtoken

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+var snoowrap = require('snoowrap');
+
 const count = 10;
-const secretKEY = 'AYNOGsGAA3YZcSpvJipuOzJBQFA'
-const clientID = 'RxxJV-lnWPtVWQ'
+const secretKEY = 'AYNOGsGAA3YZcSpvJipuOzJBQFA';
+const clientID = 'RxxJV-lnWPtVWQ';
+const refreshToken = '63627351748-h2qlzzSRyrNJyW_J6tRKcMSflSA';
+const accessToken = '63627351748-h2qlzzSRyrNJyW_J6tRKcMSflSA';
 const API = `ttps://www.reddit.com/api/v1/access_token`;
-const proxy = 'https://cors-anywhere.heroku.app.com/'
+const proxy = 'https://cors-anywhere.heroku.app.com/';
 
-async function getSubreddit() {
+
+async function scrapeSubreddit() {
     try {
-        const response = await fetch(proxy + API);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
+        const r = new Snoowrap({
+            userAgent: 'Random string',
+            clientId: clientID,
+            clientSecret: secretKEY,
+            refreshToken: refreshToken
+        });
 
-    }
+        const subreddit = await r.getSubreddit('realState');
+        const topPosts = await subreddit.getTop({
+            time: 'week',
+            limit: 10
+        });
+
+        let data = [];
+
+        topPosts.forEach((post) => {
+            data.push({
+                title: link.url,
+                text: post.title,
+                score: post.score
+            })
+        });
+    } catch (error) {
+        console.log(error);
+    };
+    console.log(data);
 }
 
-getSubreddit();
+scrapeSubreddit();
