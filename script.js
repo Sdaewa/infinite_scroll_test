56331e0d77e28ab6cd7fbc6be5f88d9b34d6f9a8
Luis Ramirez <sdaewa22@gmail.com>
2020-10-19 18:18:53 +0100

    creating DM elements

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const count = 10;
-const API = `https://api.unsplash.com/photos/random/?client_id=${}&count=${count}`;
-const secret = '';
-const client_id = '';
-const proxy = 'https://cors-anywhere.heroku.app.com/';
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
-async function getPics() {
+let photosArray = [];
+
+
+const count = 10;
+const keyAPI = 'lwpx-YU7tGq3uAZUmqwwlQt9iXpdS1MRHimbyqXKspQ';
+const API = `https://api.unsplash.com/photos/random/?client_id=${keyAPI}&count=${count}`;
+
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+    });
+}
+
+
+async function getPhotos() {
     try {
         const response = await fetch(API);
-        const data = await response.json();
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
         console.log(error);
     };
 }
 
-getPics();
+getPhotos();
