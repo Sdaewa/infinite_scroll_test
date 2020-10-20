552874b64c62512bf6e848b1a09230a323b53213
Luis Ramirez <sdaewa22@gmail.com>
2020-10-20 12:04:37 +0100

    bug solved

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const keyAPI = 'lwpx-YU7tGq3uAZUmqwwlQt9iXpdS1MRHimbyqXKspQ';
 const API = `https://api.unsplash.com/photos/random/?client_id=${keyAPI}&count=${count}`;
 
 function setAttributes(element, attributes) {
-    for (key in attributes) {
+    for (const key in attributes) {
         element.setAttribute(key, attributes[key]);
     };
 }
@@ -16,12 +16,19 @@ function setAttributes(element, attributes) {
 function displayPhotos() {
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
+
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -31,6 +38,7 @@ async function getPhotos() {
     try {
         const response = await fetch(API);
         photosArray = await response.json();
+        console.log(photosArray);
         displayPhotos();
     } catch (error) {
         alert(error);
